221db8b8007be28e9c9c41d6d1d27a4edf53d7d9
tkosht <takehito.oshita.business@gmail.com>
2023-03-03 11:40:32 +0900

    mod: __init__

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,7 @@
-from logger import *
-from params import *
-from simple_logger import *
-from ulid import *
+from .logger import *
+from .params import *
+from .simple_logger import *
+from .ulid import *
 
 __version__ = '0.9.0'
 
